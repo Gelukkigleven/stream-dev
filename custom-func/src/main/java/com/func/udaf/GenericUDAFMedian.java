5ca90af166971f3cb3bb407207878aff5b93ab0f
“Gelukkigleven” <1316805087@qq.com>
2024-12-24 08:38:08 +0800

    flink cdc

---
+++ custom-func/src/main/java/com/func/udaf/GenericUDAFMedian.java
@@ -0,0 +1,23 @@
+package com.func.udaf;
+
+import org.apache.hadoop.hive.ql.parse.SemanticException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFAverage;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFParameterInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
+
+public class GenericUDAFMedian extends GenericUDAFAverage {
+
+    public GenericUDAFMedian(){
+        super();
+    }
+
+    public GenericUDAFEvaluator getEvaluator(TypeInfo[] parameters) throws SemanticException {
+        return super.getEvaluator(parameters);
+    }
+
+    public GenericUDAFEvaluator getEvaluator(GenericUDAFParameterInfo paramInfo) throws SemanticException{
+        return super.getEvaluator(paramInfo);
+    }
+
+}
